76a56f2c6d37116a2177ed506489a598b276be0b
Rafal Dzialdowski <r.dzialdowski@globalincome.pl>
2024-11-05 07:48:43 +0100

    chore: update version to 0.0.21 and refine imports in render.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import icons from 'unplugin-icons/vite';
-import kitDocs from '@svelteness/kit-docs/node';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [icons({ compiler: 'svelte' }), kitDocs(), sveltekit()]
+	plugins: [icons({ compiler: 'svelte' }), sveltekit()]
 };
 
 export default config;
